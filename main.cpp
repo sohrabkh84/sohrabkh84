f203ce522794ca61cbf5e339d65a4e735c400586
sohrabkh84 <sohrab.hand84@gmail.com>
2023-12-25 15:32:55 +0330

    first commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int j=1;
+    int y=0;
+cin >> n ;
+int number[n];
+for (int i=0;i<n;i++){
+    cin >> number[i];
+
+    }
+    for (int s=0 ; s <n ; s++){
+    cout << number[s]<< " " ;
+    if (y==0){
+        cout << endl ;
+        j = j+1;
+        y =j;
+
+        
+    }
+    y--;
+
+
+    }
+    }
+
+
